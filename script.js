7d97abb61f25f54bd03865566b1b91529410237a
dinneshkumar9 <dinneshkumar9@gmail.com>
2023-01-30 23:37:56 +0530

    Calculator

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+const input = document.querySelector("#input");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", e => {
+    const value = e.target.innerText;
+    if (value === "Clr") {
+      input.value = "";
+    } else if (value === "=") {
+      input.value = eval(input.value);
+      if (input.value === "Infinity") {
+        input.value = "Infinity";
+      } else if (input.value === "NaN") {
+        input.value = "NaN";
+      }
+    } else {
+      input.value += value;
+    }
+  });
+});
